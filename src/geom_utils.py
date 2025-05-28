e4130791e44e5926cf63443faee2c084360c245c
Daga Panas <daga.panas@ed.ac.uk>
2025-05-28 20:00:57 +0100

    WIP sampling + annot pipeline

--- src/geom_utils.py
+++ src/geom_utils.py
@@ -1,10 +1,22 @@
 """Geometric utilitites for working with xView bounding boxes"""
 
 def get_dims( bbox):
+    """
+    To asses size of objects for possible filtering / annotating help.
+    """
     return bbox[2] - bbox[0], bbox[3] - bbox[1]
 
 def bbox_np( bbox):
+    """
+    Numpy / PIL have swapped axes wrt pyplot, and the bounding boxes in the data are in
+    pyplot ref system, will need to move back and forth between them...
+    """
     return bbox[1], bbox[0], bbox[3], bbox[2]
+
+def square_bbox_around( x, y, res):
+    x1_, x2_ = x - int( res/2), x + int(res/2)    
+    y1_, y2_ = y - int( res/2), y + int(res/2)    
+    return x1_, y1_, x2_, y2_
 
 def center_on_bbox( bbox, res, xmax, ymax, fix_edge= False):
     """
@@ -15,32 +27,35 @@ def center_on_bbox( bbox, res, xmax, ymax, fix_edge= False):
     :param res: pixel resolution of requested 'meta' bounding box
     :param xmax: dimension along x-axis in pixels
     :param ymax: dimension along y-axis in pixels
+    :param fix_edge: bool to indicate whether to move the 'meta' bbox so that it doesn't go outwith image edge
     :return bbox_meta: the bbox-surrounding res by res 'meta' bounding box; None if it goes beyond image border
     :return bbox_relative: original bounding box relative to the 
     """
     x1, y1, x2, y2 = bbox 
     x, y = x1 + int( (x2 - x1)/2), y1 + int( (y2 - y1)/2)    
-    x1_, x2_ = x - int( res/2), x + int(res/2)    
-    y1_, y2_ = y - int( res/2), y + int(res/2)    
+    x1_, y1_, x2_, y2_ = square_bbox_around( x, y, res)
     bbox_meta = [x1_, y1_, x2_, y2_]
     bbox_relative = [x1 - x1_, y1 - y1_, x2 - x1_, y2 - y1_]
     if fix_edge:
+        # if the surrounding 'meta' bbox for cropping is too close to the edge, move appropriately
         if x1_ < 0:
-            for bbox_ in [bbox_meta,bbox_relative]:
+            for ii, bbox_ in enumerate([bbox_meta,bbox_relative]):
                 for ix in [0,2]:
-                    bbox_[ix] -= x1_
+                    bbox_[ix] -= x1_ * (-1)**ii # NOTE: the relative bbox also needs re-referenced in the opposite dir
         if y1_ < 0:
-            for bbox_ in [bbox_meta,bbox_relative]:
+            for ii, bbox_ in enumerate([bbox_meta,bbox_relative]):
                 for ix in [1,3]:
-                    bbox_[ix] -= y1_
+                    bbox_[ix] -= y1_ * (-1)**ii
         if x2_ > xmax:
-            for bbox_ in [bbox_meta,bbox_relative]:
+            x2__ = x2_ - xmax
+            for ii, bbox_ in enumerate([bbox_meta,bbox_relative]):
                 for ix in [0,2]:
-                    bbox_[ix] -= x2_
+                    bbox_[ix] -= x2__ * (-1)**ii
         if y2_ > ymax:
-            for bbox_ in [bbox_meta,bbox_relative]:
+            y2__ = y2_ - ymax
+            for ii, bbox_ in enumerate([bbox_meta,bbox_relative]):
                 for ix in [1,3]:
-                    bbox_[ix] -= y2_
+                    bbox_[ix] -= y2__ * (-1)**ii
         return bbox_meta, bbox_relative
     else:
         try:
@@ -50,6 +65,9 @@ def center_on_bbox( bbox, res, xmax, ymax, fix_edge= False):
             return None, None
     
 def crop_to_bbox( image_array, bbox):
+    """
+    Crop image to just a bbox (the numpy-version of bbox)
+    """
     x1, y1, x2, y2 = bbox
     return image_array[x1:x2,:,:][:,y1:y2,:]
 
