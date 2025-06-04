4b2a61df16c69c761c45bb0572f647715a4aad44
Daga Panas <daga.panas@ed.ac.uk>
2025-06-04 20:41:08 +0100

    annot continues, check label distr

--- src/plotting.py
+++ src/plotting.py
@@ -32,3 +32,44 @@ def plot_bbox( x_ll, y_ll, x_ru, y_ru, ax= None):
     ax.plot( 
         [x_ll, x_ru, x_ru, x_ll, x_ll],[ y_ll, y_ll, y_ru, y_ru, y_ll], linewidth= 1, color= 'magenta'
     )
+    
+def to_magenta( arr, from_pix, to_pix, at_pix, axis):
+    """
+    Change a line of selected pixels to magenta (assuming a numpy array x by y by 3)
+    """
+    if axis== 'x':
+        arr[from_pix:to_pix,at_pix,0] = 255
+        arr[from_pix:to_pix,at_pix,1] = 0
+        arr[from_pix:to_pix,at_pix,2] = 255
+    elif axis=='y':
+        arr[at_pix,from_pix:to_pix,0] = 255
+        arr[at_pix,from_pix:to_pix,1] = 0
+        arr[at_pix,from_pix:to_pix,2] = 255        
+    return arr
+
+def draw_bbox( arr, bbox_in_context):
+    """
+    Draw a bounding box in magenta for display in PIL for annotation.
+    """
+    x1, y1, x2, y2 = bbox_in_context
+    crop = to_magenta( arr, x1, x2, y1, 'x')
+    crop = to_magenta( arr, y1, y2, x2, 'y')
+    crop = to_magenta( arr, x1, x2, y2, 'x')
+    crop = to_magenta( arr, y1, y2, x1, 'y')
+    return crop
+
+def mark_c( crop):
+    x1, y1, x2, y2 = (30, 20, 45, 35)
+    crop = to_magenta( crop, x1, x2, y1, 'x')
+    crop = to_magenta( crop, y1, y2, x1, 'y')
+    crop = to_magenta( crop, y1, y2, x2, 'y')
+    return crop
+
+def mark_b( crop):
+    x1, y1, x2, y2 = (60, 20, 75, 33)
+    crop = to_magenta( crop, x1, x2, y1, 'x')
+    crop = to_magenta( crop, x1+1, x2-1, y2, 'x')
+    crop = to_magenta( crop, y1 + 5, y2, x1 + int((x2-x1)/2), 'y')
+    crop = to_magenta( crop, y1, y2-2, x1, 'y')
+    crop = to_magenta( crop, y1, y2-2, x2, 'y')
+    return crop
