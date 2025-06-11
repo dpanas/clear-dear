afea312bae0aba7f7ae53d165f164eb2acd65e9b
Daga Panas <daga.panas@ed.ac.uk>
2025-06-11 19:10:53 +0100

    total mess - try add dear code + debug results - also normalising data + reviewing labels investigation

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 
 import numpy as np
 
@@ -17,13 +18,23 @@ def to_numpy( image_tensor):
     Convenience util for analysis of single images when read by PIL or torch/vision.
     """
     image_array = np.asarray( image_tensor)
+    if image_array.max() < 1:
+        image_array = (image_array * 255).astype(np.uint8)
+    
     if image_array.shape[0] == 3:
-        return np.swapaxes( np.swapaxes( np.asarray( image_tensor), 0, 1), 1, 2)
+        return np.swapaxes( np.swapaxes( image_array, 0, 1), 1, 2)
     elif image_array.shape[-1] == 3:
         return image_array
     else:
         raise Exception(f'Weird shape for an image: {image_array.shape}')
 
+def to_from_pickle( file_path, content= None):
+    mode = 'wb' if content is not None else 'rb'
+    with open( file_path, mode) as stream:
+        if mode.startswith('r'):
+            return pickle.load( stream)
+        pickle.dump( content, stream)
+        
 def to_from_text( file_path, content= None, encoding= 'utf-8', as_bytes= False):
     mode = 'w' if content is not None else 'r'
     if as_bytes:
---
+++ src/dear/utils.py
@@ -0,0 +1,193 @@
+import os
+import matplotlib
+import torch
+
+import numpy as np
+import pandas as pd
+
+from PIL import Image
+from torchvision import datasets, transforms
+from torch.utils.data import TensorDataset, DataLoader
+
+#matplotlib.use('Agg')
+
+def draw_recon(x, x_recon):
+    x_l, x_recon_l = x.tolist(), x_recon.tolist()
+    result = [None] * (len(x_l) + len(x_recon_l))
+    result[::2] = x_l
+    result[1::2] = x_recon_l
+    return torch.FloatTensor(result)
+
+
+def make_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def denorm(x):
+    out = (x + 1) / 2
+    return out.clamp_(0, 1)
+
+
+def write_config_to_file(config, save_path):
+    with open(os.path.join(save_path, 'config.txt'), 'w') as file:
+        for arg in vars(config):
+            file.write(str(arg) + ': ' + str(getattr(config, arg)) + '\n')
+
+
+def make_dataloader(args):
+
+    test_loader = None
+    train_loader = None
+    if args.dataset == 'celeba':
+
+        trans_f = transforms.Compose([
+            transforms.CenterCrop(128),
+            transforms.Resize((args.image_size, args.image_size)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        train_set = datasets.CelebA(args.data_dir, split='train', download=False, transform=trans_f)
+        train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, pin_memory=False,
+                                                   drop_last=True, num_workers=4)
+
+    elif 'pendulum' in args.dataset:
+        train_set = dataload_withlabel(args.data_dir, image_size = args.image_size,
+                                       mode='train', sup_prop=args.sup_prop)
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=4)
+    
+    else:
+        train_set = dataload_withlabel( 
+            args.data_dir, image_size= args.image_size, label_file= args.label_file, mode= '', sup_prop= args.sup_prop
+        )
+        train_loader = DataLoader(
+            train_set, batch_size= args.batch_size, shuffle= True, drop_last= True, num_workers= 4
+        )
+
+    return train_loader, test_loader
+
+
+def get_device( sagan_obj= None):
+    if sagan_obj is not None:
+        if not sagan_obj.config.disable_cuda and torch.cuda.is_available():
+            print("CUDA is available!")
+            sagan_obj.device = torch.device('cuda')
+            sagan_obj.config.dataloader_args['pin_memory'] = True
+        else:
+            print("Cuda is NOT available, running on CPU.")
+            sagan_obj.device = torch.device('cpu')
+        if torch.cuda.is_available() and sagan_obj.config.disable_cuda:
+            print("WARNING: You have a CUDA device, so you should probably run without --disable_cuda")
+    else:
+        if torch.cuda.is_available():
+            return torch.device('cuda')
+        else:
+            return torch.device('cpu')
+
+
+class dataload_withlabel(torch.utils.data.Dataset):
+    def __init__( self, root, label_file=None, image_size=64, mode="train", sup_prop=1., num_sample=0):
+        # label_file: 'pendulum_label_downstream.txt'
+        self.root = root
+
+        self.label_file = label_file
+        if label_file is not None:
+            if label_file == 'labels.csv':
+                label_path = os.path.join( self.root, label_file)
+                print( label_path)
+                self.attrs_df = pd.read_csv( label_path)
+                
+                self.img_paths = self.attrs_df['image'].values
+                
+                self.labels = self.attrs_df.drop('image', axis=1).values
+
+                self.transforms = transforms.Compose([
+                    transforms.Resize((image_size, image_size)),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ])
+
+                self.n = len(self.img_paths)
+                np.random.seed(2)
+                self.available_label_index = np.random.choice(self.n, int(self.n * sup_prop), replace=False)
+
+            else:
+                self.attrs_df = pd.read_csv(os.path.join( self.root, label_file))
+                self.split_df = pd.read_csv(os.path.join( self.root, label_file))
+                splits = self.split_df['partition'].values
+                split_map = {
+                    "train": 0,
+                    "valid": 1,
+                    "test": 2,
+                    "all": None,
+                }
+                split = split_map[verify_str_arg(mode.lower(), "split",
+                                                ("train", "valid", "test", "all"))]
+                mask = slice(None) if split is None else (splits == split)
+                self.mask = mask
+                np.random.seed(2)
+                if num_sample > 0:
+                    idxs = [i for i, x in enumerate(mask) if x]
+                    not_sample = np.random.permutation(idxs)[num_sample:]
+                    mask[not_sample] = False
+                self.attrs_df = self.attrs_df.values
+                self.attrs_df[self.attrs_df == -1] = 0
+                self.attrs_df = self.attrs_df[mask][:, [0,1,2,3,6]]
+                self.imglabel = torch.as_tensor(self.attrs_df.astype(np.float))
+                self.img_paths = []
+                for i in range(3):
+                    mode1 = list(split_map.keys())[i]
+                    root1 = self.root + mode1
+                    imgs = os.listdir(root1)
+                    self.img_paths += [os.path.join( self.root, mode1, k) for k in imgs]
+                self.img_paths = np.array(self.img_paths)[mask]
+        else:
+            root = self.root + mode
+            imgs = os.listdir(root)
+            self.img_paths = [os.path.join(root, k) for k in imgs]
+            try:
+                # that was originally in DEAR for the 'pendulum' dataset, that has labels in image names
+                self.imglabel = [list(map(float, k[:-4].split("_")[1:])) for k in imgs]
+            except Exception:
+                self.imglabel = [[-1,] for i in range(len(imgs))]
+        self.transforms = transforms.Compose([transforms.Resize((image_size, image_size)),transforms.ToTensor()])
+        np.random.seed(2)
+        self.n = len(self.img_paths)
+        self.available_label_index = np.random.choice(self.n, int(self.n * sup_prop), replace=0)
+
+    def __getitem__(self, idx):
+        if self.label_file == 'labels.csv':
+            img_path = os.path.join( self.root, 'images', self.img_paths[idx])  
+           # print(f"Loading image from: {img_path}")  
+            label = self.labels[idx]
+
+            if idx not in self.available_label_index:
+                label = torch.zeros_like(torch.tensor(label)) - 1  
+
+            pil_img = Image.open(img_path).convert('RGB')
+            data = self.transforms(pil_img)
+
+            label = torch.tensor(label, dtype=torch.float32)
+        else:
+            img_path = self.img_paths[idx]
+            if not (idx in self.available_label_index):
+                label = torch.zeros(4).long() - 1
+            else:
+                if self.label_file is None:
+                    label = torch.from_numpy(np.asarray(self.imglabel[idx]))
+                else:
+                    label = self.imglabel[idx]
+            pil_img = Image.open(img_path).convert('RGB')
+            array = np.array(pil_img)
+            array1 = np.array(label)
+            label = torch.from_numpy(array1)
+            data = torch.from_numpy(array)
+            if self.transforms:
+                data = self.transforms(pil_img)
+            else:
+                pil_img = np.asarray(pil_img).reshape(96,96,3)
+                data = torch.from_numpy(pil_img)
+        return data, label.float(), img_path
+
+    def __len__(self):
+        return len(self.img_paths)
